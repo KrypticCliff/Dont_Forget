6fa0110a2e3fdd831cf5c02dae824542e2df6e99
KrypticCliff <Clifton.raw@gmail.com>
2018-10-12 14:53:51 -0700

    Cascade Build Update

--- Cascade_Build.cpp
+++ Cascade_Build.cpp
@@ -12,6 +12,7 @@ namespace fs = boost::filesystem;
 int main(int argc, char** argv) {
      fs::path p("./img/neg_img/");
      std::vector<cv::Mat> img_v;
+     cv::Mat img;
      //std::vector<std::string> img_filenames;
 
      // Path existance check
@@ -24,13 +25,20 @@ int main(int argc, char** argv) {
 
      // Iterate through folder findes img files and converts to Grayscale
      try {
-          for (; begin != end; begin++) {
+          for (int counter = 0; begin != end; begin++, counter++) {
                if (fs::is_regular_file(begin->path())) {
-               int img_counter = 0;
-               //std::cout << begin->path().string() << " has been selected.\n";
-               // Places grayscale MAT into img_v vector
-               img_v.push_back(cv::imread(begin->path().string(), cv::IMREAD_GRAYSCALE));
-               cv::resize(img_v[img_counter], img_v[img_counter], cv::Size(100,100));
+                    std::string str_counter = std::to_string(counter);
+                    std::cout << begin->path().string() << " has been selected.\n";
+
+                    // Places grayscale MAT into img_v vector
+                    img_v.push_back(cv::imread(begin->path().string(), cv::IMREAD_GRAYSCALE));
+
+                    // Resize and save as numbered jpg
+                    cv::resize(img_v[counter], img_v[counter], cv::Size(100,100));
+                    cv::imwrite("./img/cvt_neg_img/" + str_counter + ".jpg", img_v[counter]);
+
+                    std::cout << counter << "\n";
+                    printf("Converted...\n\n");
                }
           }
      }
